336f539096b06746e1e403e33c53911d9f0ac368
chfoers <cf328622@fh-muenster.de>
2019-09-02 10:29:45 +0200

    test3

--- GraphGsb/src/main/java/de/materna/Graph/test.java
+++ GraphGsb/src/main/java/de/materna/Graph/test.java
@@ -2,6 +2,6 @@ package de.materna.Graph;
 
 public class test {
 	  public static void main(String[] args) {
-	    System.out.println("Git is coolSS");
+	    System.out.println("Git is cool");
 	  }
 	}
